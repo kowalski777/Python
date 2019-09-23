d5a121b5a877e29105f2b8b380388fd25e96f3a2
Esteban Garriga <estebangarriga@hotmail.com>
2019-09-23 10:26:58 -0300

    Create conjuntos.py

---
+++ conjuntos.py
@@ -0,0 +1,7 @@
+def union_conjuntos(conjunto_a,conjunto_b):
+  print("\nLa union de A y B es {}\n".format(conjunto_a.union(conjunto_b)))
+
+def interseccion_conjuntos(conjunto_a, conjunto_b):
+  print("\nLa interseccion de A y B es {}\n".format(conjunto_a.intersection(conjunto_b)))
+
+def diferencia
